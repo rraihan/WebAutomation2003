70096b9a95a44d60cd9f84585cff6a58a4dab1a0
Sam Smith <dialraihan@gmail.com>
2022-06-01 11:21:20 -0400

    First commit WebAutomation2003

---
+++ src/test/java/abc_practices/GmailSigninTest.java
@@ -0,0 +1,31 @@
+package abc_practices;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class GmailSigninTest {
+
+	public static void main(String[] args) {
+		
+		System.setProperty( "webdriver.chrome.driver", "./drivers/chromedriver.exe");
+		WebDriver driver = new ChromeDriver();
+		
+		driver.get("https://accounts.google.com/accountchooser/signinchooser?flowName=GlifWebSignIn&flowEntry=AccountChooser");
+		driver.manage().window().maximize();
+		
+		
+		
+		driver.findElement(By.id("identifierId")).sendKeys("dialraihan@gmail.com");
+		
+	
+		driver.findElement(By.xpath("//*[@id='identifierNext']/div/button")).click();
+		
+	}
+
+
+
+
+	}
+
+
